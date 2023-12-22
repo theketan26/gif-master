7614f96f98dbd6901570aafe126d138f64d511a6
theketan26 <ketangsolanki129@gmail.com>
2023-12-22 14:06:40 +0530

    Loader added

--- src/components/GifSearch.jsx
+++ src/components/GifSearch.jsx
@@ -4,8 +4,7 @@
 import React, { useState, useEffect } from 'react';
 import { searchGifs } from '../services/giphy';
 import './globals.css'
-// import Loader from 'react-loader-spinner';
-// import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import Image from 'next/image';
 
 
 const GifSearch = () => {
@@ -16,6 +15,7 @@ const GifSearch = () => {
 
 
     const handleSearch = async () => {
+        setGifs([]);
         setIsLoading(true);
         const results = await searchGifs(query);
         setGifs(results);
@@ -33,21 +33,36 @@ const GifSearch = () => {
     };
 
 
-    useEffect(() => {
-        if (query !== '') {
-            handleSearch();
-        }
-    }, [query]);
+    // useEffect(() => {
+    //     if (query !== '') {
+    //         handleSearch();
+    //     }
+    // }, [query]);
 
 
     return (
         <div className = 'flex flex-col bg-stone-900/25 m-20 rounded-xl py-5'>
+
+
             <div className = 'mt-5 w-1/2 self-center flex justify-between'>
                 <input className = 'w-full me-5 self-center h-10 text-xl px-4 rounded-xl min-w-[300px]'
                     placeholder = 'Search'
                     type="text" value={ query } onChange={(e) => setQuery(e.target.value)} />
                 <button className = 'p-2 text-xl bg-stone-200 w-28 self-center rounded-full text-stone-800 font-bold bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-orange-900 via-amber-100 to-orange-900'
                     onClick={handleSearch}>Search</button>
+            </div>
+
+
+            <div className = 'h-4 mt-2 self-center'>
+                {
+                    isLoading && 
+                        <img
+                            src = 'images/Spinner.svg'
+                            width = { 40 }
+                            height = { 40 }
+                            alt = 'Loading'
+                        />
+                }
             </div>
 
             <div className = 'w-4/5 mt-20 self-center flex flex-row flex-wrap justify-evenly'>
